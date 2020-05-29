399f2f7a2fc2f13b30c76e1c6a30ee4e3103740c
Amardip <mahisakapal@gmail.com>
2020-05-30 00:12:24 +0530

    python add

--- python/test.py
+++ python/test.py
@@ -1,1 +1,12 @@
 print("'jay shri 'manjunath 'blass me '")
+## this is the commit you can print "word " or 'word' or combint 
+## you can't " "this" " or ' 'this' ' mud mixmatch  
+
+
+#############################################################
+########################## Now Escape Sequences #############
+#############################################################
+
+## to " " in to anther " " you have add \  after evry " "
+
+print ("this exmple for \"testing \"  " )
