35e2dd81ef5de6bb2363b9d9e8fd5364d258b1a3
Amardip <mahisakapal@gmail.com>
2020-05-30 00:52:19 +0530

    new update py

--- python/test.py
+++ python/test.py
@@ -9,4 +9,26 @@ print("'jay shri 'manjunath 'blass me '")
 
 ## to " " in to anther " " you have add \  after evry " "
 
+##  \' = single quote
+##  \\" = duble quote
+##  \\  = backshlash
+##  \n  = new line
+##  \t  = tab
+##  \b  = baksapce   
+
+
 print ("this exmple for \"testing \"  " )
+
+print ('i\'m amardip')
+
+print("this is use to when you need  backshalsh in end of word use 2 backshalsh \\")
+
+print ("use \\\\ for \\ to print ")
+
+print ("line A \nline B")
+
+print ("check your tab\t  this to tab in your txt")
+
+print ("this help you to get backsapce\b check this mahi\b ") # it remove last liter or give backsapash 
+
+
