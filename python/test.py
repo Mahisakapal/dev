940b6ac388e65846d711d8cb195030f778cd3f75
Amardip <mahisakapal@gmail.com>
2020-05-30 01:48:09 +0530

    add emoji in python

--- python/test.py
+++ python/test.py
@@ -32,3 +32,29 @@ print ("check your tab\t  this to tab in your txt")
 print ("this help you to get backsapce\b check this mahi\b ") # it remove last liter or give backsapash 
 
 
+#############################################################
+############################# this is exercise #############
+#############################################################
+print ("\\\\")
+
+print("/\\/\\/\\/\\")
+
+print('  \\"  \\n  \\t ' "\\' " )
+
+print ("\\\" \\n \\t \\\' ")
+
+
+## it u use "r" in start of your string it's not reacte 
+
+print(r"this is A \n  this is B\t")
+
+#############################################################
+############################# now we look emoji #############
+#############################################################
+
+# if you want to print emoji go to this url & copy th code replyce + with 000  https://unicode.org/emoji/charts/full-emoji-list.html
+
+print("\U0001F618")
+
+print("\U0001F602")
+
