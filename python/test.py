022fbf2ffc3cc156370280b5b00c530d27f25aea
Amardip <mahisakapal@gmail.com>
2020-05-30 17:58:05 +0530

    after int got error

--- python/test.py
+++ python/test.py
@@ -89,4 +89,55 @@ print(12**4)  # to get power of number
 
 print(2** 0.5) # in this we got long digit output to fix this we user round 
 
-print (round(2** 0.5,000) )# we user round(2** 0.5, Number of digit you want in our case we take 2(00)
+print (round(2** 0.5, 3 ) ) # we user round(2** 0.5, Number of digit you want in our case we take 2
+
+#############################################################
+####################### now we look vireable ################
+#############################################################
+
+F_name = "amardip "
+
+M_nub =20
+
+L_name = "sakapal "   # this is my vireable
+
+Full_name =  F_name + " " + L_name
+
+print ( Full_name + str (M_nub))
+
+print(F_name * 3 ) # this will print string 3 time 
+
+
+#############################################################
+#################### now we look user input  ################
+#############################################################
+
+#when we take a input from user it is alwsay string 
+# if you want to use user input as int you have convert it int(vireableName )
+
+name = input("type your name ") # we get input & save this in vireable name 
+
+age = input("what's your age ")
+
+print("wellcome " + name + ' \n great your now '+  age  ) 
+
+u_after10 = int(age) + 10     # in this we can cover sting to int & add number 
+
+print("you after ten " + str(u_after10))  # here we take 
+################################################################################
+################################################################################
+
+
+age_of_expri = int(input("what's your age ")) # her we take user input in int 
+
+u_after1 = (age_of_expri) + 10     # in this we can cover sting to int & add number 
+
+print("cool if you fight 2020 will have good day when you " + str(u_after1))  # here we take int & conver it  string 
+
+
+
+
+
+
+
+
