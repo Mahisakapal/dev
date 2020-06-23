1602a9975b452a3057fac9bda09e9bfed6928c63
Amardip <mahisakapal@gmail.com>
2020-06-23 17:57:01 +0530

    new py curese

---
+++ python/new coursh/New cures.py
@@ -0,0 +1,17 @@
+'''
+this webner 
+'''
+
+# idinterfer is function , varable, class 
+# vierble rule 
+# must not start with number  not support specalcharcter only accept " _ " underscore _ 
+# reservier keyword not allow   
+
+x =10 
+y =23
+tmp_vlu = None
+print(f"x ={x} & y = {y} bewfor swap the value " )
+tmp_vlu = x 
+x=y 
+y=tmp_vlu
+print(f"x ={x} & y = {y} after swap the value " )
